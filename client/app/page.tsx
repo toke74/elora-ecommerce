0420137d068e6b67bac189c52645ce16c739095b
toke wak <tokeabaye@gmail.com>
2024-03-15 21:00:14 -0400

    clinet set up

---
+++ client/app/page.tsx
@@ -0,0 +1,14 @@
+import Heading from "./utils/Heading";
+
+export default function Home() {
+  return (
+    <main>
+      <Heading
+        title="Elora"
+        description="Elora is the online platform for selling and buying products over the internet "
+        keywords="shopping  cloth shoes "
+      />
+      <h1>Home Page</h1>
+    </main>
+  );
+}
---
+++ client/app/utils/Heading.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+interface HeadingProps {
+  title: string;
+  description?: string;
+  keywords?: string;
+}
+
+const Heading = ({ title, description, keywords }: HeadingProps) => {
+  return (
+    <div>
+      <title>{title}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+    </div>
+  );
+};
+
+export default Heading;
